00f3ef77bb4285a4b956c3edab4fab9f23edc855
Reihaneh Mahjour <rihaneh763@gmail.com>
2022-08-29 13:58:57 +0430

    add class ApplicationConstant in pakage service

---
+++ src/service/ApplicationConstant.java
@@ -0,0 +1,10 @@
+package service;
+
+import java.sql.Connection;
+
+public class ApplicationConstant {
+    private static Connection connection = new DBHelper().connect();
+    public static Connection getConnection(){
+        return connection;
+    }
+}
