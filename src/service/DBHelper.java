cf515ec1c192e6537a38f67d90a24863d73f85f2
Reihaneh Mahjour <rihaneh763@gmail.com>
2022-08-29 17:57:02 +0430

    add class UserReppository for CRUD operation

--- src/service/DBHelper.java
+++ src/service/DBHelper.java
@@ -5,7 +5,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBHelper {
-    private final static String URL = "jdbc:postgresql://127.0.0.1:5432/maktab";
+    private final static String URL = "jdbc:postgresql://127.0.0.1:5432/Article";
     private final static String USERNAME = "postgres";
     private final static String PASSWORD = "Rm@38936";
     private Connection connection;
